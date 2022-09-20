9b7ad191e19fb22e73a57f30e3b33c8bcb964fdf
Szilágyi László <raptile666@gmail.com>
2022-09-21 00:24:48 +0200

    404 Error page finished

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Pic404 from '../assets/404.png';
+import '../styles/PageNotFound.css'
 
 const PageNotFound = () => {
   return (
-    <div>404 - PageNotFound</div>
+    <div className='container-404'>
+      <div className='pic404' style={{ backgroundImage: `url(${Pic404})` }}></div>
+      <div className='text404'>
+        <h1>Page cannot be found</h1>
+      </div>
+    </div>
   )
 }
 
