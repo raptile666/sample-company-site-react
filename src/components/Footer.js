9e191d8a582df1d211651e1a18bfaf196d254148
Szilágyi László <raptile666@gmail.com>
2022-09-20 23:34:46 +0200

    Home page finished. Minor modification on Footer and index css files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,27 +15,29 @@ const Footer = () => {
 
         <div className='contact'>
           <h4>Contact Info</h4>
-            <div>
-              <span> <Phone /></span> <span><a href="tel:+36123456789">+36 12 345 6789</a></span>
-            </div>
-            <div>
-              <span> <MailOutline /></span> <span><a href="mailto:john@sample.sam">john@sample.sam</a></span>
-            </div>
+          <div className='cont'>
+            <div> <Phone /></div> <div><a href="tel:+36123456789">+36 12 345 6789</a></div>
+          </div>
+          <div className='cont'>
+            <div> <MailOutline /></div> <div><a href="mailto:john@sample.sam">john@sample.sam</a></div>
+          </div>
         </div>
 
         <div className='pages'>
           <h4>Pages</h4>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/services">Services</Link>
-          <Link to="/projects">Projects</Link>
-          <Link to="/career">Career</Link>
-          <Link to="/contact">Contact</Link>
+          <div className='pagelink'>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/services">Services</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/career">Career</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
         </div>
       </div>
 
       <div className='copyright'>
-        <span>2022 © All Rights Reserved | László Szilágyi</span> <span><LinkedIn /></span> <span><GitHub /></span>
+        <div>2022 © All Rights Reserved | László Szilágyi</div> <div><LinkedIn /></div> <div><GitHub /></div>
       </div>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import '../styles/Home.css'
+import '../styles/Home.css';
+import HomePic from '../assets/home-bg.jpg';
+import { Link } from 'react-router-dom';
+import { LocalFireDepartment, Engineering, SportsMotorsports } from '@mui/icons-material';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='main'>
+      <div className='detail' style={{ backgroundImage: `url(${HomePic})` }}>
+        <h1>The Big Title</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, temporibus provident? Asperiores id beatae illo repellendus earum ea reiciendis vero iusto provident, facere deleniti consequatur laborum quaerat facilis itaque odit voluptatem repudiandae sint? Ullam mollitia fugiat ipsa ipsum, asperiores hic dolores ab distinctio quae aspernatur nesciunt porro quis, aliquam dolore?</p>
+        <Link to='/contact'><button>Contact</button></Link>
+      </div>
+
+      <div className='table'>
+        <div className='cell'>
+          <div className='pic'><LocalFireDepartment /></div>
+        <div className='pictext'>
+          <h1>First</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo veritatis eum officiis quis!</p>
+          </div>
+        </div>
+
+        <div className='cell'>
+          <div className='pic'><Engineering /></div>
+          <div className='pictext'>
+            <h1>Second</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi exercitationem perferendis fugiat quaerat iste quam?</p>
+          </div>
+        </div>
+
+        <div className='cell'>
+          <div className='pic'><SportsMotorsports /></div>
+          <div className='pictext'>
+            <h1>Third</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, praesentium!</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
