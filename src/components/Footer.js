04f939a475ef2e2cf90ea48bc2834a2746a9738b
Szilágyi László <raptile666@gmail.com>
2022-09-20 17:48:16 +0200

    NavBar and Footer finished. index.css modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+import TextLogo from '../assets/textlogo.png';
+import '../styles/Footer.css';
+import {Phone, MailOutline, LinkedIn, GitHub} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      
+      <div className='footer'>
+        <div className='logo'>
+          <img src={TextLogo} alt="TextLogo" id='textlogo' />
+        </div>
+
+        <div className='contact'>
+          <h4>Contact Info</h4>
+            <div>
+              <span> <Phone /></span> <span><a href="tel:+36123456789">+36 12 345 6789</a></span>
+            </div>
+            <div>
+              <span> <MailOutline /></span> <span><a href="mailto:john@sample.sam">john@sample.sam</a></span>
+            </div>
+        </div>
+
+        <div className='pages'>
+          <h4>Pages</h4>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/career">Career</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+
+      <div className='copyright'>
+        <span>2022 © All Rights Reserved | László Szilágyi</span> <span><LinkedIn /></span> <span><GitHub /></span>
+      </div>
+    </div>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logo.png';
+import '../styles/NavBar.css'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+        <div className='nav-left'>
+            <img src={Logo} alt="logo" id='logo'/>
+        </div>
+
+        <div className="nav-right">
+            <Link to="/" className='borderLink'>Home</Link>
+            <Link to="/about" className='borderLink'>About</Link>
+            <Link to="/services" className='borderLink'>Services</Link>
+            <Link to="/projects" className='borderLink'>Projects</Link>
+            <Link to="/career" className='borderLink'>Career</Link>
+            <Link to="/contact">Contact</Link>
+        </div>
+    </div>
   )
 }
 
