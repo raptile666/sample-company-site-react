8e52e6e6d28137d57aa328f6e73b6a044134162b
Szilágyi László <raptile666@gmail.com>
2022-09-25 12:32:38 +0200

    Career page finished

--- src/pages/Career.js
+++ src/pages/Career.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Career.css';
 
 const Career = () => {
+
+
+
   return (
-    <div>Career</div>
+    <div className='careerMain'>
+      <h1>The Position We are looking for</h1>
+      <div className='divider'></div>
+
+      <h4>The task you have to do</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nam veniam rerum eaque saepe corrupti recusandae velit et incidunt similique temporibus illo doloribus facere ab, magni voluptatem autem nobis accusamus consectetur quidem voluptate ex enim. Ullam minus molestiae voluptatem, itaque expedita, modi veritatis, laudantium maiores nemo natus magnam mollitia veniam quasi necessitatibus libero ut non voluptate. Aperiam quas a minima hic totam sed necessitatibus, non quod nesciunt voluptates cumque rem. Quam repellendus nisi id sequi porro quia ullam maiores tenetur veritatis, qui nihil. Nihil suscipit sit adipisci incidunt, sint, officia esse vel provident ab culpa eaque quod fuga voluptates est labore nam, laudantium aperiam molestias? Exercitationem, in!</p>
+
+      <h4>Expectations</h4>
+      <ul>
+        <li>lorem</li>
+        <li>ipsum</li>
+        <li>dolor</li>
+        <li>sit</li>
+        <li>amet</li>
+      </ul>
+
+      <h4>Good you can</h4>
+      <ul>
+        <li>consectetur</li>
+        <li>repellendus</li>
+        <li>veritatis</li>
+        <li>nihil</li>
+        <li>veniam</li>
+      </ul>
+
+      <h4>We give</h4>
+      <ul>
+        <li>laudantium</li>
+        <li>suscipit</li>
+        <li>ullam</li>
+        <li>itaque</li>
+        <li>sequi</li>
+        <li>facere</li>
+        <li>accusamus</li>
+      </ul>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit <strong><a href="mailto:john@sample.sam">john@sample.sam</a></strong>. Nesciunt at ipsam placeat! Magni laudantium veritatis maiores tenetur. Autem fugiat ipsam corporis?</p>
+    </div>
   )
 }
 
