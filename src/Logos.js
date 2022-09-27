256eddde8305ad8d67fa0d0ea4905aa7960c8327
Szilágyi László <raptile666@gmail.com>
2022-09-27 23:42:03 +0200

    Projects finished

---
+++ src/Logos.js
@@ -0,0 +1,65 @@
+import Logo1 from './assets/logo-01.png';
+import Logo2 from './assets/logo-02.png';
+import Logo3 from './assets/logo-03.png';
+import Logo4 from './assets/logo-04.png';
+import Logo5 from './assets/logo-05.png';
+import Logo6 from './assets/logo-06.png';
+import Logo7 from './assets/logo-07.png';
+import Logo8 from './assets/logo-08.png';
+import Logo9 from './assets/logo-09.png';
+import Logo10 from './assets/logo-10.png';
+import Logo11 from './assets/logo-11.png';
+import Logo12 from './assets/logo-12.png';
+
+const Logos = [
+    {
+        "image": Logo1,
+        "text": "Logo 1"
+    },
+    {
+        "image": Logo2,
+        "text": "Logo 2"
+    },
+    {
+        "image": Logo3,
+        "text": "Logo 3"
+    },
+    {
+        "image": Logo4,
+        "text": "Logo 4"
+    },
+    {
+        "image": Logo5,
+        "text": "Logo 5"
+    },
+    {
+        "image": Logo6,
+        "text": "Logo 6"
+    },
+    {
+        "image": Logo7,
+        "text": "Logo 7"
+    },
+    {
+        "image": Logo8,
+        "text": "Logo 8"
+    },
+    {
+        "image": Logo9,
+        "text": "Logo 9"
+    },
+    {
+        "image": Logo10,
+        "text": "Logo 10"
+    },
+    {
+        "image": Logo11,
+        "text": "Logo 11"
+    },
+    {
+        "image": Logo12,
+        "text": "Logo 12"
+    },
+]
+
+export default Logos
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Logos from '../Logos';
+import '../styles/Projects.css';
 
 const Projects = () => {
+  
   return (
-    <div>Projects</div>
+    <div className='projectMain'>
+      <h1>Our Biggest Partners</h1>
+      <div className='projectContainer'>
+        {Logos.map((logo) => {
+          return(
+            <div className='logoContainer'>
+              <img src={logo.image} alt={logo.text}
+                onMouseEnter={( prop ) => 
+                { prop.target.nextElementSibling.className = 'show' }} 
+                
+                onMouseLeave={( prop ) => 
+                { prop.target.nextElementSibling.className = 'hide' }} />
+
+              <h2 className='hide'>{logo.text}</h2>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
